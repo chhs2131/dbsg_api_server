d07a4f6143f6249b0da77307c88d22fb4851176d
Basak <chgy2131@naver.com>
2024-01-09 01:09:28 +0900

    add: 테스트 가능한 형태의 네이버뉴스 API 코드 추가

--- src/test/java/kr/co/dbsg/api/example/NaverNewsExample.java
+++ src/test/java/kr/co/dbsg/api/example/NaverNewsExample.java
@@ -1,21 +1,41 @@
 package kr.co.dbsg.api.example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.web.client.RestClient;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class NaverNewsExample {
-    private RestClient restClient;
-
-    @BeforeEach
-    public void before() {
-        restClient = RestClient.builder()
-                .baseUrl("https://openapi.naver.com/v1/search/news.json\t")
-                .build();
-    }
-
     @Test
-    public void example() {
-        restClient;
+    void example() {
+        String clientId = "-------------";
+        String clientSecret = "------------";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-Naver-Client-Id", clientId);
+        headers.set("X-Naver-Client-Secret", clientSecret);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://openapi.naver.com/v1/search/news.json")
+                .queryParam("query", "IPO")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "sim");
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = new RestTemplate().exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            String responseBody = response.getBody();
+            System.out.println(responseBody);
+        } else {
+            System.out.println("Request failed with status code: " + response.getStatusCode());
+        }
     }
 }

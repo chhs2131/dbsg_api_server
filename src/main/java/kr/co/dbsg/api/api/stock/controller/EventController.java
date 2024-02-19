38c5db4122731ef6e03a409baf0fd34fab018cbc
Basak <chgy2131@naver.com>
2024-02-20 02:51:22 +0900

    feat(event): Event 목록 요청 Model을 별도의 DTO로 분리 및 유효성 검증 진행

--- src/main/java/kr/co/dbsg/api/api/stock/controller/EventController.java
+++ src/main/java/kr/co/dbsg/api/api/stock/controller/EventController.java
@@ -1,19 +1,19 @@
 package kr.co.dbsg.api.api.stock.controller;
 
-import java.time.LocalDate;
+import jakarta.validation.Valid;
 import kr.co.dbsg.api.api.stock.dto.EventDto.EventResponse;
+import kr.co.dbsg.api.api.stock.dto.EventRequestDto;
 import kr.co.dbsg.api.api.stock.service.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,20 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final EventService eventService;
 
-    @GetMapping("")
-    public ResponseEntity<Page<EventResponse>> getEvents(
-        @RequestParam(name = "page", defaultValue = "0", required = false) int page,
-        @RequestParam(name = "size", defaultValue = "10", required = false) int size,
-        @RequestParam(name = "startDate", defaultValue = "#{T(java.time.LocalDateTime).now().minusDays(7)}", required = false) LocalDate startDate,
-        @RequestParam(name = "endDate", defaultValue = "#{T(java.time.LocalDateTime).now()}", required = false) LocalDate endDate
-    ) {
-        // TODO validation
-
-        // logic
-        Pageable pageable = PageRequest.of(page, size);
+    @GetMapping
+    public ResponseEntity<Page<EventResponse>> getEvents(@ParameterObject @Valid @ModelAttribute EventRequestDto request) {
+        Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
 
         return ResponseEntity.ok(
-                eventService.getEvents(startDate, endDate, pageable)
+                eventService.getEvents(request.getStartDate(), request.getEndDate(), pageable)
                 .map(EventResponse::from)
         );
     }
---
+++ src/main/java/kr/co/dbsg/api/api/stock/dto/EventRequestDto.java
@@ -0,0 +1,29 @@
+package kr.co.dbsg.api.api.stock.dto;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EventRequestDto {
+    @Min(0)
+    @Parameter(description = "조회할 페이지 번호 (기본값: 0)")
+    private int page = 0;
+
+    @Min(1)
+    @Max(100)
+    @Parameter(description = "한 번에 표시할 검색 결과 개수 (기본값: 10, 최댓값: 100)")
+    private int size = 10;
+
+    @Parameter(description = "일정 조회 시작 일시 (형식: YYYY-MM-DD, 기본값: 오늘로부터 7일전)")
+    private LocalDate startDate = LocalDate.now().minusDays(7);
+
+    @Parameter(description = "일정 조회 종료 일시 (형식: YYYY-MM-DD, 기본값: 오늘)")
+    private LocalDate endDate = LocalDate.now();
+}

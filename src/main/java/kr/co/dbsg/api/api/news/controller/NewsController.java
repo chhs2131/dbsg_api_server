161a6fcd410bd98e639f7e167a86b57342772d37
Basak <chgy2131@naver.com>
2024-02-20 03:06:54 +0900

    feat(news): News 목록 요청시 유효성 검증 진행

--- src/main/java/kr/co/dbsg/api/api/news/controller/NewsController.java
+++ src/main/java/kr/co/dbsg/api/api/news/controller/NewsController.java
@@ -1,5 +1,9 @@
 package kr.co.dbsg.api.api.news.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import kr.co.dbsg.api.api.news.dto.NewsResponse;
 import kr.co.dbsg.api.api.news.service.NewsService;
@@ -18,6 +22,7 @@ public class NewsController {
 
     @GetMapping("")
     public ResponseEntity<List<NewsResponse>> getNews(
+            @Valid @NotBlank @Size(min = 1, max = 20) @Parameter(description = "검색할 키워드를 입력합니다. (글자수 제한: 1~20글자)")
             @RequestParam(name = "title", defaultValue = "IPO", required = false) String title
     ) {
         return ResponseEntity.ok(newsService.getNews(title));

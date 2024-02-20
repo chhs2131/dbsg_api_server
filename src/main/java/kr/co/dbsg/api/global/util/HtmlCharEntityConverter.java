def5ef1264ae009d2027c6e3c06de3a5ad377985
Basak <chgy2131@naver.com>
2024-02-20 15:34:08 +0900

    refactor: HtmlUnescape 동작을 Spring 내장 라이브러리를 사용하도록 수정

--- src/main/java/kr/co/dbsg/api/global/util/HtmlCharEntityConverter.java
+++ src/main/java/kr/co/dbsg/api/global/util/HtmlCharEntityConverter.java
@@ -1,11 +1,12 @@
 package kr.co.dbsg.api.global.util;
 
-import org.apache.commons.text.StringEscapeUtils;
+import org.springframework.web.util.HtmlUtils;
 
 public class HtmlCharEntityConverter {
     public static String unescape(String input) {
         String result = input.replaceAll("<[^>]*>", ""); // HTML 태그 제거
-        return StringEscapeUtils.unescapeHtml4(result);
+        result = result.replaceAll("&nbsp;", " ");  // &nbsp; 전처리
+        return HtmlUtils.htmlUnescape(result);
     }
 
     public static String replace(String input) {
---
+++ src/test/java/kr/co/dbsg/api/example/CommonsText.java
@@ -0,0 +1,17 @@
+package kr.co.dbsg.api.example;
+
+import kr.co.dbsg.api.global.util.HtmlCharEntityConverter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CommonsText {
+    @Test
+    void HTML문자가_정상적으로_제거되나() {
+        String html = "안녕하세요&nbsp;&nbsp;바삭입니다.<br>오늘도&nbsp;좋은&nbsp;하루&nbsp;되세요! &quot;<p>행복하세요!</p>&quot; &#39;네, 행복합니다!&#39; &lt;사랑합니다&gt; &amp; 감사합니다.";
+
+        String result = HtmlCharEntityConverter.unescape(html);
+
+        String expected = "안녕하세요  바삭입니다.오늘도 좋은 하루 되세요! \"행복하세요!\" '네, 행복합니다!' <사랑합니다> & 감사합니다.";
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}

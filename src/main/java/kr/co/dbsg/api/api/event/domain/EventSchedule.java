ed3ce784dce2229dc9a17ac028b85907989239d2
Basak <chgy2131@naver.com>
2024-03-24 21:03:48 +0900

    refactor: Event Schedule 검증 로직 추가

--- src/main/java/kr/co/dbsg/api/api/event/domain/EventSchedule.java
+++ src/main/java/kr/co/dbsg/api/api/event/domain/EventSchedule.java
@@ -2,11 +2,10 @@ package kr.co.dbsg.api.api.event.domain;
 
 import java.time.LocalDate;
 import kr.co.dbsg.api.api.event.domain.type.EventStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Getter
-@AllArgsConstructor
 public class EventSchedule {
     private final LocalDate forecastStart;
     private final LocalDate forecastEnd;
@@ -15,6 +14,39 @@ public class EventSchedule {
     private final LocalDate refund;
     private final LocalDate debut;
     private final LocalDate eventCancel;
+
+    public EventSchedule(final LocalDate forecastStart, final LocalDate forecastEnd, final LocalDate subscriptionStart,
+                         final LocalDate subscriptionEnd, final LocalDate refund, final LocalDate debut,
+                         final LocalDate eventCancel) {
+        // TODO 일정에 대해 더 상세한 검증 필요 (모든 일정에 대해 도메인 조건이 검증되지 못함)
+        // forecastStart가 forecastEnd보다 앞일 수 없다.
+        Assert.isTrue(forecastStart == null || forecastEnd == null || !forecastStart.isAfter(forecastEnd),
+            "forecastStart must be before or equal to forecastEnd");
+
+        // subscriptionStart가 subscriptionEnd보다 앞일 수 없다.
+        Assert.isTrue(subscriptionStart == null || subscriptionEnd == null || !subscriptionStart.isAfter(subscriptionEnd),
+            "subscriptionStart must be before or equal to subscriptionEnd");
+
+        // forecastEnd가 subscriptionStart보다 앞일 수 없다.
+        Assert.isTrue(forecastEnd == null || subscriptionStart == null || !forecastEnd.isAfter(subscriptionStart),
+            "forecastEnd must be before or equal to subscriptionStart");
+
+        // subscriptionEnd가 refund보다 앞일 수 없다.
+        Assert.isTrue(subscriptionEnd == null || refund == null || !subscriptionEnd.isAfter(refund),
+            "subscriptionEnd must be before or equal to refund");
+
+        // refund가 debut보다 앞일 수 없다.
+        Assert.isTrue(refund == null || debut == null || !refund.isAfter(debut),
+            "refund must be before or equal to debut");
+
+        this.forecastStart = forecastStart;
+        this.forecastEnd = forecastEnd;
+        this.subscriptionStart = subscriptionStart;
+        this.subscriptionEnd = subscriptionEnd;
+        this.refund = refund;
+        this.debut = debut;
+        this.eventCancel = eventCancel;
+    }
 
     public EventStatus getStatus() {
         LocalDate now = LocalDate.now();

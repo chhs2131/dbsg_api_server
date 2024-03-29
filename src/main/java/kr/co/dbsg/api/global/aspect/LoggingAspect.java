6417c882bf3f0f68deee6f487c15594e5b0a9a31
Basak <chgy2131@naver.com>
2024-03-29 16:38:13 +0900

    refactor: LoggingAspect가 Local 및 Dev 환경에서만 동작하도록 수정

--- src/main/java/kr/co/dbsg/api/global/aspect/LoggingAspect.java
+++ src/main/java/kr/co/dbsg/api/global/aspect/LoggingAspect.java
@@ -6,15 +6,16 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Slf4j
+@Profile({"local", "dev"})
 @Aspect
 @Component
 public class LoggingAspect {
-//    @Pointcut("execution(* kr.co.dbsg.api.api.*.controller.*(..))")
 
     @Pointcut("execution(* kr.co.dbsg.api.api..*Controller.*(..))")
     public void controllerPointcut() {

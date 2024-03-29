d461b9c6906e435543b1ece60269b05dfd48b944
Basak <chgy2131@naver.com>
2024-03-29 17:57:36 +0900

    refactor: NaverNewsRepository가 RestClient를 통해 통신하도록 수정

--- src/main/java/kr/co/dbsg/api/api/news/repository/NaverNewsRepository.java
+++ src/main/java/kr/co/dbsg/api/api/news/repository/NaverNewsRepository.java
@@ -1,7 +1,7 @@
 package kr.co.dbsg.api.api.news.repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 import kr.co.dbsg.api.api.news.entity.NewsEntity;
 import kr.co.dbsg.api.global.config.properties.NaverApiProperty;
 import kr.co.dbsg.api.global.util.HtmlCharEntityConverter;
@@ -9,71 +9,62 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.client.RestClient;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Primary
 @Repository
 @RequiredArgsConstructor
 public class NaverNewsRepository implements NewsRepository {
     private final NaverApiProperty naverApiProperty;
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public List<NewsEntity> findAllByTitle(String title) {
-        String baseUrl = naverApiProperty.baseUrl();
+        String requestUri = naverApiProperty.baseUrl() + "/v1/search/news.json";
         String clientId = naverApiProperty.clientId();
         String clientSecret = naverApiProperty.clientSecret();
 
+        String uriWithParam = UriComponentsBuilder.fromHttpUrl(requestUri)
+            .queryParam("query", title)
+            .queryParam("display", 10)
+            .queryParam("start", 1)
+            .queryParam("sort", "sim")
+            .toUriString();
+
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("X-Naver-Client-Id", clientId);
         headers.set("X-Naver-Client-Secret", clientSecret);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl + "/v1/search/news.json")
-                .queryParam("query", title)
-                .queryParam("display", 10)
-                .queryParam("start", 1)
-                .queryParam("sort", "sim");
+        return getArticles(uriWithParam, headers).stream()
+            .map(naverNews -> NewsEntity.builder()
+                .id(0L)
+                .title(removeHtmlEntity(naverNews.title))
+                .url(naverNews.originallink)
+                .desc(naverNews.description)
+                .publishedAt(toLocalDateTime(naverNews.pubDate))
+                .build())
+            .toList();
+    }
 
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<String> response = new RestTemplate().exchange(
-                builder.toUriString(),
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            String responseBody = response.getBody();
-
-            try {
-                List<NaverArticle> newsEntities = objectMapper.readValue(responseBody, NaverNews.class).items;
-
-                return newsEntities.stream()
-                        .map(naverNews -> NewsEntity.builder()
-                                .id(0L)
-                                .title(removeHtmlEntity(naverNews.title))
-                                .url(naverNews.originallink)
-                                .desc(naverNews.description)
-                                .publishedAt(toLocalDateTime(naverNews.pubDate))
-                                .build())
-                        .toList();
-            } catch(JsonProcessingException e) {
-                e.printStackTrace();
-                throw new IllegalArgumentException("Json 파싱 에러");  //TODO exception 정의
-            }
-        } else {
-            throw new IllegalArgumentException("Request failed with status code: " + response.getStatusCode());  //TODO exception 정의
-        }
+    private static List<NaverArticle> getArticles(final String requestUri, final HttpHeaders headers) {
+        //TODO NPE 발생가능
+        return RestClient.create().get()
+            .uri(requestUri)
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(httpHeaders -> httpHeaders.addAll(headers))
+            .retrieve()
+            .toEntity(NaverNews.class)
+            .getBody()  //TODO 응답이 2xx가 아닌경우 처리
+            .items;
     }
 
     private LocalDateTime toLocalDateTime(String naverTimeFormat) {
@@ -88,7 +79,7 @@ public class NaverNewsRepository implements NewsRepository {
 
     @Data
     @NoArgsConstructor
-    static class NaverNews {
+    private static class NaverNews {
         private String lastBuildDate;
         private int total;
         private int start;
@@ -98,7 +89,7 @@ public class NaverNewsRepository implements NewsRepository {
 
     @Data
     @NoArgsConstructor
-    static class NaverArticle {
+    private static class NaverArticle {
         private String title;
         private String originallink;
         private String link;

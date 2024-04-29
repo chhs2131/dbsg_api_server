79868616623c2ec101ca86f02c88ec2a718d1467
Basak <chgy2131@naver.com>
2024-04-29 17:33:58 +0900

    fix: News API End-Point 수정 ("/v1/news")

--- src/main/java/kr/co/dbsg/api/api/news/controller/NewsController.java
+++ src/main/java/kr/co/dbsg/api/api/news/controller/NewsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/news")
+@RequestMapping("/v1/news")
 @RequiredArgsConstructor
 public class NewsController {
     private final NewsService newsService;

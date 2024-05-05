1ef433ce02f7ed79becbbf6499826907f7e467c5
Basak <chgy2131@naver.com>
2024-05-05 16:37:35 +0900

    feat: 권한이 필요한 EndPoint들을 Swagger에서도 명시적으로 나타내도록 수정 - LoginUser 어노테이션에 스웨거 관련 설정 추가

--- src/main/java/kr/co/dbsg/api/global/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/dbsg/api/global/config/SwaggerConfiguration.java
@@ -3,38 +3,40 @@ package kr.co.dbsg.api.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
+    public static final String SECURITY_SCHEME_NAME = "Authorization";
+
     @Bean
     public OpenAPI openAPI() {
+        // Swagger 정보 설정
         Info info = new Info()
-            .version("v1.0.0")
+            .version("v1.0.1")
             .title("공모주토피아 API")
             .description("https://www.github.com/chhs2131");
 
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-            .name(jwt)
-            .type(SecurityScheme.Type.HTTP)
-            .scheme("bearer")
-            .bearerFormat("JWT")
-        );
+        // security 설정
+        Components components = new Components()
+            .addSecuritySchemes(SECURITY_SCHEME_NAME, getSecuritySchemesItem(SECURITY_SCHEME_NAME));
 
+        // Bean 등록
         return new OpenAPI()
             .info(info)
-            .addSecurityItem(securityRequirement)
             .components(components);
     }
 
-//    @Bean
-//    @Profile("dev")
-//    public GroupedOpenApi disable() {
-//        return GroupedOpenApi.builder().build();
-//    }
+    private SecurityScheme getSecuritySchemesItem(final String jwt) {
+        return new SecurityScheme()
+            .name(jwt)
+            .type(SecurityScheme.Type.HTTP)
+            .in(In.HEADER)
+            .description("API Token")
+            .scheme("bearer")
+            .bearerFormat("JWT");
+    }
 }
--- src/main/java/kr/co/dbsg/api/global/resolver/LoginUser.java
+++ src/main/java/kr/co/dbsg/api/global/resolver/LoginUser.java
@@ -1,5 +1,9 @@
 package kr.co.dbsg.api.global.resolver;
 
+import static kr.co.dbsg.api.global.config.SwaggerConfiguration.SECURITY_SCHEME_NAME;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +11,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Operation(security = @SecurityRequirement(name = SECURITY_SCHEME_NAME))
 public @interface LoginUser {
 }
